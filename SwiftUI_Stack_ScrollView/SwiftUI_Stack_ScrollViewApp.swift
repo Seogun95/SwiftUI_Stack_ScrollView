734ab3a0cf17649caeffde397f3c1806cbe155cd
SeonJungKim <ksj083895@gmail.com>
2021-03-10 18:52:02 +0500

    Initial Commit

---
+++ SwiftUI_Stack_ScrollView/SwiftUI_Stack_ScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Stack_ScrollViewApp.swift
+//  SwiftUI_Stack_ScrollView
+//
+//  Created by 김선중 on 2021/03/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Stack_ScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
